57c83ad94a12a57938dd1802a9318883a261ea5c
olatide <olatidejoseph17@gmail.com>
2023-07-15 16:59:23 -0700

    exec c is fun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+/**
+ * main - main func
+ * Return: 0
+ * Description: this
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
