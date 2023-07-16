26019719f1f2ee61bc4cecd4c717177885f8250b
olatide <olatidejoseph17@gmail.com>
2023-07-16 08:40:26 -0700

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    /* Generate lowercase letters from 'a' to 'z' */
+	char lowercase[26];
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		lowercase[i] = 'a' + i;
+	}
+    /* Accessing and printing the lowercase letters */
+	for (i = 0; i < 26; i++)
+	{
+		putchar(lowercase[i]);
+		if (i == 25)
+			putchar('\n');
+	}
+
+	return (0);
+}
