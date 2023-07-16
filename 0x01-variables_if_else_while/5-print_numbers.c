ae753a1e1a0844c25da64e6c7e5cbb5ff40b8fc4
olatide <olatidejoseph17@gmail.com>
2023-07-16 10:44:31 -0700

    next

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printsnumber to ten
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i=0;
+
+	while(i<=9)
+	{
+		printf("%d",i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
