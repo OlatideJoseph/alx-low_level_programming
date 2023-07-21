0d99e144c30792caaade421a5317764fab5b175d
olatide <olatidejoseph17@gmail.com>
2023-07-20 17:44:57 -0700

    committed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,6 @@
 
 void positive_or_negtive(int i);
 int largest_number(int a, int b, int c);
-:x
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif /* MAIN_H */
