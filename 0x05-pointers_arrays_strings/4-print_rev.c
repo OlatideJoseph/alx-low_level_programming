15e840023df5ae85a332e9bdc3c066fea3dbc270
olatide <olatidejoseph17@gmail.com>
2023-07-25 20:09:20 -0700

    rev out

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@
  */
 void print_rev(char *s)
 {
-        int i;
+	int i;
 
-        for (i = strlen(s) - 1; i >= 0; i--)
-        {
-                putchar(s[i]);
-        }
-        putchar('\n');
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
 }
 
