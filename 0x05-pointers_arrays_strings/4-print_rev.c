ff0e0717a08e5c56bde6c9ac9e86e581b66e7dfc
olatide <olatidejoseph17@gmail.com>
2023-07-25 20:03:02 -0700

    rev out

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - idn
+ * @s: char
+ * Return:NULL
+ */
+void print_rev(char *s)
+{
+        int i;
+
+        for (i = strlen(s) - 1; i >= 0; i--)
+        {
+                putchar(s[i]);
+        }
+        putchar('\n');
+}
+
