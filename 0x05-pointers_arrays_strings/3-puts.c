383a4062b5bba59ad07f122c6f1421cce7455528
olatide <olatidejoseph17@gmail.com>
2023-07-25 19:21:16 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#includr "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - acts as put
+ * @str: takes in a str
+ * Return: NULL
+ */
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		puts(*s[i]);
+	}
+	puts('/n');
