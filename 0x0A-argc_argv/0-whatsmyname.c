20e25a4b5202b6036101fcffe775fe1c3be4a3f6
Olatide Joseph Adeniyi <olatidejoseph17@gmail.com>
2023-08-05 04:11:18 +0100

    args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,13 @@
 #include "stdio.h"
 /**
  * main - prints it name
+ * @argc: accepts int
+ * @argv: accepts cli args
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
