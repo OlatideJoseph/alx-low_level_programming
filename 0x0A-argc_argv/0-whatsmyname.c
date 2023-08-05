b718e40abd9752a14bdaa926b4b38e98bbe4de0c
Olatide Joseph Adeniyi <olatidejoseph17@gmail.com>
2023-08-05 03:48:04 +0100

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+/**
+ * main - prints it name
+ * Return: 0
+ */
+int main(int argc, char *argc[])
+{
+	printf("%s\n", argc[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+A directory that deqls with command line arguments
