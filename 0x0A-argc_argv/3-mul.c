7f130da9b0929f1518d47e59a21475e9556d8315
Olatide Joseph Adeniyi <olatidejoseph17@gmail.com>
2023-08-05 13:08:30 +0100

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,22 +3,22 @@
 /**
  * main - ttyhh
  * @argc: this
- * @arg: main
+ * @argv: main
  * Return: certain type int
  */
 int main(int argc, char *argv[])
 {
 	int num1, num2, product;
 
-	if (argc != 3) /* check if the number of arguments is correct */
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	num1 = atoi(argv[1]); /* convert the first argument to an integer */
-	num2 = atoi(argv[2]); /* convert the second argument to an integer */
-	product = num1 * num2; /* calculate the product of the two numbers */
-	printf("%d\n", product); /* print the product followed by a new line */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 * num2;
+	printf("%d\n", product);
 	return (0);
 }
