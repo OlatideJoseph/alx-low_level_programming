53b8fc99a0813d97d26d2f3393e216d6d8797859
Olatide Joseph Adeniyi <olatidejoseph17@gmail.com>
2023-08-05 04:07:12 +0100

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,12 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "main.h"
 /**
  * main - prints it name
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
