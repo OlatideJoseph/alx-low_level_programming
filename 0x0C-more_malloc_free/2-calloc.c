967b04c40fcdf1a0691c71664ecb59e0f322aa35
Olatide Joseph Adeniyi <olatidejoseph17@gmail.com>
2023-10-09 01:42:17 +0100

    puff

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero.
+ * @nmemb: The number of elements in the array.
+ * @size: The size of each element in bytes.
+ *
+ * Return: A pointer to the allocated and initialized memory.
+ *         If nmemb or size is 0, or if malloc fails, returns NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	/* Initialize the allocated memory to zero */
+	for (unsigned int i = 0; i < nmemb * size; i++)
+		*((char *)ptr + i) = 0;
+
+	return (ptr);
+}
