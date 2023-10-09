5b1b0d6474fd1bd19d00f2dbf11841a111b47828
Olatide Joseph Adeniyi <olatidejoseph17@gmail.com>
2023-10-09 01:16:07 +0100

    rrr

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - func
+ * @b: an unsugned int
+ * Return: void
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL) exit(98);
+
+	return (ptr);
+}
